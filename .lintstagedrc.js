5375daeb17a5728418211cf3111fa8463aaf2916
doinki <doinki94@gmail.com>
2022-05-22 19:47:51 +0900

    🚀

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
 const buildEslintCommand = (filenames) =>
-  `next lint -d @ --fix --file ${filenames
+  `yarn lint -d @ --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`;
 
 module.exports = {
   '*.@(j|t)s?(x)': [buildEslintCommand],
-  '*.json': 'prettier --write',
+  '*.json': 'yarn prettier --write',
 };

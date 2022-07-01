d7f85a95fc44a331a708c12830ac5a8af7441bbd
doinki <doinki94@gmail.com>
2022-07-02 00:40:42 +0900

    🚀

--- jest.config.js
+++ jest.config.js
@@ -7,24 +7,11 @@ const createJestConfig = nextJest({ dir: '.' });
  */
 const customJestConfig = {
   collectCoverageFrom: ['src/**/*.@(j|t)s?(x)'],
-  coveragePathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/src\\/types/',
-  ],
   moduleNameMapper: {
-    '^@/api(.*)': '<rootDir>/src/api$1',
-    '^@/atoms(.*)': '<rootDir>/src/components/atoms$1',
-    '^@/constants(.*)': '<rootDir>/src/constants$1',
-    '^@/hooks(.*)': '<rootDir>/src/hooks$1',
-    '^@/icons(.*)': '<rootDir>/src/components/atoms/icons$1',
-    '^@/molecules(.*)': '<rootDir>/src/components/molecules$1',
-    '^@/organisms(.*)': '<rootDir>/src/components/organisms$1',
-    '^@/styles(.*)': '<rootDir>/src/styles$1',
-    '^@/templates(.*)': '<rootDir>/src/components/templates$1',
-    '^@/types(.*)': '<rootDir>/src/types$1',
-    '^@/utils(.*)': '<rootDir>/src/utils$1',
+    '^@/(.*)': '<rootDir>/src/$1',
+    '^src/(.*)': '<rootDir>/src/$1',
   },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jsdom',
 };
 
